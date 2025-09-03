559de084907af8ec0d65fb331e6b5da637e0adc9
Akshit4340 <24ce01005@iitbbs.ac.in>
2025-09-04 01:02:04 +0530

    Added home get route

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,13 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+try {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  app.get("/", (req, res) => {
+    res.send("Hello World!");
+  });
+} catch (error) {
+  console.error("Error starting server:", error);
+}
